211a09e552a552dbf23f37224e9d6fca7d53fc63
Asinsa <asinsa.g@gmail.com>
2023-02-02 19:07:11 +0000

    Added upload button to upload java files only

--- src/main/java/com/example/testgenerator/views/main/MainView.java
+++ src/main/java/com/example/testgenerator/views/main/MainView.java
@@ -3,9 +3,12 @@ package com.example.testgenerator.views.main;
 import com.example.testgenerator.views.MainLayout;
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.upload.Upload;
+import com.vaadin.flow.component.upload.receivers.MemoryBuffer;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
@@ -17,8 +20,28 @@ public class MainView extends HorizontalLayout {
 
     private TextField name;
     private Button sayHello;
+    private Button addQuestion;
 
     public MainView() {
+        MemoryBuffer buffer = new MemoryBuffer();
+        Upload upload = new Upload(buffer);
+        upload.setAcceptedFileTypes("text/x-java-source,java", ".java");
+
+        Button uploadButton = new Button("Upload Question");
+        uploadButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+
+        upload.setUploadButton(uploadButton);
+
+        // Disable the upload button after the file is selected
+        // Re-enable the upload button after the file is cleared
+        upload.getElement().addEventListener("max-files-reached-changed", event -> {
+            boolean maxFilesReached = event.getEventData()
+                    .getBoolean("event.detail.value");
+            uploadButton.setEnabled(!maxFilesReached);
+        }).addEventData("event.detail.value");
+
+        add(upload);
+
         name = new TextField("Your name");
         sayHello = new Button("Say hello");
         sayHello.addClickListener(e -> {
