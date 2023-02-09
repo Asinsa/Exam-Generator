af2611224c743b1b455a596408a39ef54858ed38
Asinsa <asinsa.g@gmail.com>
2023-02-09 13:33:55 +0000

    Added list showing the names of all questions and the date they were uploaded

--- src/main/java/com/example/testgenerator/views/main/MainView.java
+++ src/main/java/com/example/testgenerator/views/main/MainView.java
@@ -4,29 +4,34 @@ import com.example.testgenerator.views.MainLayout;
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.component.upload.MultiFileReceiver;
 import com.vaadin.flow.component.upload.Upload;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
 import elemental.json.Json;
+import org.apache.commons.io.FilenameUtils;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 @PageTitle("Main")
 @Route(value = "main", layout = MainLayout.class)
 @RouteAlias(value = "", layout = MainLayout.class)
 public class MainView extends HorizontalLayout {
 
-    private TextField name;
-    private Button sayHello;
+    private Button uploadButton;
 
     public MainView() {
+
+        // Upload questions
         File uploadFolder = new File("src/main/questions");
         if (!uploadFolder.exists()) {
             uploadFolder.mkdirs();
@@ -49,24 +54,42 @@ public class MainView extends HorizontalLayout {
 
         upload.setAcceptedFileTypes("text/x-java-source,java", ".java");
 
-        Button uploadButton = new Button("Upload Question");
+        uploadButton = new Button("Upload Question");
         uploadButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        uploadButton.addClickShortcut(Key.ENTER);
 
         upload.setUploadButton(uploadButton);
 
-        add(upload);
 
-        name = new TextField("Your name");
-        sayHello = new Button("Say hello");
-        sayHello.addClickListener(e -> {
-            Notification.show("Hello " + name.getValue());
-        });
-        sayHello.addClickShortcut(Key.ENTER);
+        // View All Question Names
+        List<ArrayList<String>> questions = new ArrayList<>();
 
+        File[] files = new File("src/main/questions").listFiles();
+
+        for (File file : files) {
+            if (file.isFile()) {
+                String filename = file.getName();
+                if (FilenameUtils.getExtension(filename).equals("java")) {
+                    ArrayList<String> question = new ArrayList<>();
+
+                    question.add(filename.substring(0, filename.lastIndexOf('.')));
+                    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+                    question.add(sdf.format(file.lastModified()));
+
+                    questions.add(question);
+                }
+            }
+        }
+
+        Grid<ArrayList<String>> grid = new Grid<>();
+        grid.setItems(questions);
+        grid.addColumn(a -> a.get(0)).setHeader("Question Name").setSortable(true);
+        grid.addColumn(a -> a.get(1)).setHeader("Upload Date").setSortable(true);
+
+        // Layouts
         setMargin(true);
-        setVerticalComponentAlignment(Alignment.END, name, sayHello);
-
-        add(name, sayHello);
+        add(upload);
+        add(grid);
     }
 
 
